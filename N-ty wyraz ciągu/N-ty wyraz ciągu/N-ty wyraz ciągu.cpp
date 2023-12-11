f275d4981fb5ff2b8b094f2e70f1ff9ed8af4b07
Oliwer Mielcarek <rejzorx@gmail.com>
2023-12-11 13:55:43 +0100

    ok

---
+++ N-ty wyraz ciągu/N-ty wyraz ciągu/N-ty wyraz ciągu.cpp
@@ -0,0 +1,27 @@
+﻿#include <iostream>
+using namespace std;
+int fibonacci(int n) {
+    if (n <= 1) {
+        return n;
+    }
+    else {
+        return fibonacci(n - 1) + fibonacci(n - 2);
+    }
+}
+
+int main() {
+    int n;
+
+    cout << "Podaj numer wyrazu w ciagu Fibonacciego: ";
+    cin >> n;
+
+    if (n < 0) {
+        cout << "Numer wyrazu musi byc nieujemny." << endl;
+    }
+    else {
+        int result = fibonacci(n);
+       cout << "Wyraz o numerze " << n << " w ciagu Fibonacciego to: " << result << endl;
+    }
+
+    return 0;
+}
